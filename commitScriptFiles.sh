fbfb95835b2539293bbe47fa961dad5c8c9aa2b9
Laura Davis-Robeson <ldavisrobeson@forumone.com>
2014-10-10 16:41:54 -0700

    Automatic snapshot

--- commitScriptFiles.sh
+++ commitScriptFiles.sh
@@ -3,4 +3,4 @@
 # Copy & paste this into Terminal to set required permissions and run the script:
 # chmod a+x ~/Dropbox/Local\ Dev\ Config/scripts/commitChangedFiles.sh && ~/Dropbox/Local\ Dev\ Config/scripts/commitChangedFiles.sh
 
-cd ~/Dropbox/Local\ Dev\ Config/scripts && git add . && git commit -m "Automatic snapshot" && git push origin master
+cd ~/Dropbox/Local\ Dev\ Config/scripts && git add -u . && git commit -m "Automatic snapshot" && git push origin master
