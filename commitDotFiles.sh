f491076fba4061c2edc0039cdf716c22ebfa2935
Laura Davis-Robeson <ldavisrobeson@forumone.com>
2014-10-10 16:43:18 -0700

    Automatic snapshot by Alfred. Yay, automation!

--- commitDotFiles.sh
+++ commitDotFiles.sh
@@ -1,6 +1,6 @@
 # Used by Alfred to automatically commit changes in important local dev files
 #
 # Copy & paste this into Terminal to set required permissions and run the script:
-# chmod a+x ~/Dropbox/Local\ Dev\ Config/scripts/commitChangedFiles.sh && ~/Dropbox/Local\ Dev\ Config/scripts/commitChangedFiles.sh
+# chmod a+x ~/Dropbox/Local\ Dev\ Config/scripts/commitChangedFiles.sh && ~/Dropbox/Local\ Dev\ Config/scripts/commitDotFiles.sh
 
-cd ~/Dropbox/Local\ Dev\ Config/dot\ files && git add -u . && git commit -m "Automatic snapshot" && git push origin master
+cd ~/Dropbox/Local\ Dev\ Config/dot\ files && git add -u . && git commit -m "Automatic snapshot by Alfred. Yay, automation!" && git push origin master
