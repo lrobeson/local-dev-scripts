4f0e521eb3c443471b7132d47560f578e512ea28
Laura Davis-Robeson <ldavisrobeson@forumone.com>
2014-10-14 11:37:58 -0700

    Automatic snapshot by Alfred. Yay, automation!

---
+++ commitAlfredFiles.sh
@@ -0,0 +1,6 @@
+# Used by Alfred workflow to automatically commit changes in Alfred directory.
+#
+# To run this script manually, copy & paste this into Terminal:
+# ~/Dropbox/Local-Dev-Config/scripts/commitAlfredFiles.sh
+
+cd ~/Dropbox/Local-Dev-Config/Alfred && git add -u . && git commit -m "Automatic snapshot by Alfred. Yay, automation!" && git push origin master
--- commitScriptFiles.sh
+++ commitScriptFiles.sh
@@ -3,4 +3,4 @@
 # To run this script manually, copy & paste this into Terminal:
 # ~/Dropbox/Local-Dev-Config/scripts/commitScriptFiles.sh
 
-cd ~/Dropbox/Local-Dev-Config/scripts && git add -u . && git commit -m "Automatic snapshot by Alfred. Yay, automation!" && git push origin master
+cd ~/Dropbox/Local-Dev-Config/scripts && git add . && git add -u . && git commit -m "Automatic snapshot by Alfred. Yay, automation!" && git push origin master
