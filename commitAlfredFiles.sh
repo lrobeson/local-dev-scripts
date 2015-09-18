c0ba1a2d7a19dde02509e1d1ca1e8b1daff7702d
Laura Davis-Robeson <ldavisrobeson@forumone.com>
2015-09-18 09:26:13 -0700

    Automatic snapshot by Alfred. Yay, automation!

--- commitAlfredFiles.sh
+++ commitAlfredFiles.sh
@@ -1,6 +1,6 @@
 # Used by Alfred workflow to automatically commit changes in Alfred directory.
 #
 # To run this script manually, copy & paste this into Terminal:
-# ~/Google Drive/Local Dev/scripts/commitAlfredFiles.sh
+# ~/Google\ Drive/Local\ Dev/scripts/commitAlfredFiles.sh
 
 cd /Users/ldavis-robeson/Google\ Drive/Local\ Dev/Alfred && git add . && git add -u . && git commit -m "Automatic snapshot by Alfred. Yay, automation!" && git push origin master
