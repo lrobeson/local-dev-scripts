957c190c9d782f9dba363821b9b9bc535c322907
Laura Davis-Robeson <ldavisrobeson@forumone.com>
2015-12-28 15:33:11 -0800

    Automatic snapshot by Alfred. Yay, automation!

---
+++ runMackup.sh
@@ -0,0 +1,6 @@
+# Used by Alfred workflow to automatically run Mackup.
+#
+# To run/test this script manually, copy & paste this into Terminal:
+# ~/Google\ Drive/Local\ Dev/scripts/runMackup.sh
+
+cd /Users/ldavis-robeson/Google\ Drive/Local\ Dev && mackup backup --force
