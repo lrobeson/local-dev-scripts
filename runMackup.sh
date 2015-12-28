82c4cb650598b03cde8c300314f65de77fa48326
Laura Davis-Robeson <ldavisrobeson@forumone.com>
2015-12-28 15:47:43 -0800

    Automatic snapshot by Alfred. Yay, automation!

--- runMackup.sh
+++ runMackup.sh
@@ -3,4 +3,4 @@
 # To run/test this script manually, copy & paste this into Terminal:
 # ~/Google\ Drive/Local\ Dev/scripts/runMackup.sh
 
-cd /Users/ldavis-robeson/Google\ Drive/Local\ Dev && mackup backup --force
+mackup backup --force
